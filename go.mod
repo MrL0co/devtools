22dab9371fbe45476359883dc8202a02a1a78299
Mrl0co <beheer.loco@gmail.com>
2022-01-11 20:17:17 +0100

    attempt for autocompletion

--- go.mod
+++ go.mod
@@ -16,12 +16,15 @@ replace internal/logging => ./internal/logging
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
-	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mouuff/go-rocket-update v1.5.2 // indirect
+	github.com/posener/complete v1.2.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sanbornm/go-selfupdate v0.0.0-20210106163404-c9b625feac49 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/posener/complete"
 	"os"
 	"sort"
+	"strings"
 
 	. "internal/logging"
 	"internal/update"
@@ -14,47 +17,102 @@ import (
 var Version = "development"
 var Build = ""
 
+func PrintCompletions(app *cli.App) {
+	var cmpl []string
+	for _, appFlag := range app.Flags {
+		if documentFlag, ok := appFlag.(cli.DocGenerationFlag); ok {
+			for _, name := range documentFlag.Names() {
+				cmpl = append(
+					cmpl, fmt.Sprintf(" '-%s[%s]'", name, documentFlag.GetUsage()))
+			}
+		}
+
+	}
+	fmt.Print(
+		fmt.Sprintf("_arguments -s %s", strings.Join(cmpl, " ")),
+	)
+}
+
 func main() {
-	Log.SetLogLevel(Debug)
+	Log.SetLogLevel(Info)
 	updater := update.NewSelfUpdater(Version)
 	var versionFlag bool
+	app := cli.NewApp()
+	app.Usage = "Manage your development environment"
 
-	app := &cli.App{
-		Flags: []cli.Flag{
-			&cli.BoolFlag{
-				Name:        "version",
-				Aliases:     []string{"v"},
-				Usage:       "prints the version and exit",
-				Destination: &versionFlag,
-			},
+	//app.EnableBashCompletion = true
+
+	// create the complete command
+	cmp := complete.New(
+		app.Name,
+		complete.Command{Flags: complete.Flags{"-name": complete.PredictAnything}},
+	)
+
+	// AddFlags adds the completion flags to the program flags,
+	// in case of using non-default flag set, it is possible to pass
+	// it as an argument.
+	// it is possible to set custom flags name
+	// so when one will type 'self -h', he will see '-complete' to install the
+	// completion and -uncomplete to uninstall it.
+	cmp.CLI.InstallName = "complete"
+	cmp.CLI.UninstallName = "uncomplete"
+	cmp.AddFlags(nil)
+
+	// parse the flags - both the program's flags and the completion flags
+	flag.Parse()
+
+	// run the completion, in case that the completion was invoked
+	// and ran as a completion script or handled a flag that passed
+	// as argument, the Run method will return true,
+	// in that case, our program have nothing to do and should return.
+	if cmp.Complete() {
+		return
+	}
+
+	app.Flags = []cli.Flag{
+		&cli.BoolFlag{
+			Name:        "version",
+			Aliases:     []string{"v"},
+			Usage:       "prints the version and exit",
+			Destination: &versionFlag,
 		},
-		Name:  "devtools",
-		Usage: "Manage your development environment",
-		Action: func(c *cli.Context) error {
-			if c.Bool("version") {
-				fmt.Println(updater.GetVersion())
-				return nil
-			}
-			Log.Info("boom! I say!")
+	}
 
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("version") {
+			fmt.Println(updater.GetVersion())
 			return nil
-		},
-		Commands: []*cli.Command{
-			{
-				Name:    "complete",
-				Aliases: []string{"c"},
-				Usage:   "complete a task on the list",
-				Action: func(c *cli.Context) error {
-					return nil
-				},
+		}
+		Log.Info("boom! I say!")
+
+		return nil
+	}
+
+	stop := false
+	app.Commands = []*cli.Command{
+		{
+			Name:  "complete",
+			Usage: "use for zsh auto completion",
+			Action: func(c *cli.Context) error {
+				PrintCompletions(app)
+				stop = true
+				return nil
 			},
-			{
-				Name:    "add",
-				Aliases: []string{"a"},
-				Usage:   "add a task to the list",
-				Action: func(c *cli.Context) error {
-					return nil
-				},
+		},
+		{
+			Name:    "init",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				return nil
 			},
 		},
 	}
@@ -67,7 +125,10 @@ func main() {
 		Log.Fatal("app.run failed: ", err)
 	}
 
-	if versionFlag {
+	help := flag.Bool("help", false, "")
+	h := flag.Bool("h", false, "")
+
+	if versionFlag || *help || *h || stop {
 		return
 	}
 
